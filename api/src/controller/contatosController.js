22f35e3dd194f7243eb10f8b0c3f2a2545537b45
Lílian Teixeira <lilianhteixeira@gmail.com>
2020-11-13 17:56:07 -0300

    Criando as rotas getById e deleteContatoById

--- api/src/controller/contatosController.js
+++ api/src/controller/contatosController.js
@@ -9,7 +9,7 @@ const getAll = (request, response) => {
             return response.status(500).send(error);
         }else{
             return response.status(200).json({
-                mensagem: "GET com sucesso",
+                mensagem: "GET com sucesso.",
                 contatos
             })
         }
@@ -25,14 +25,68 @@ const addContato = (request, response) => {
             return response.status(400).send(error);
         }else{
             return response.status(200).send({
-                mensagem: "POST com sucesso",
+                mensagem: "POST com sucesso.",
                 contato
             });
         }
     });
 }
 
+const getByNome = (request, response) => {
+    const nome = request.params.nome;
+
+    contatoCollections.find({"nome":nome},(error, nomeContato) => {
+        if(error){
+            return response.status(500).send(error);
+        }else if(nomeContato == ""){
+            return response.status(400).send({
+                mensagem: "O contato informado não foi encontrado.",
+                nomeContato
+            });
+        }else{
+            return response.status(200).send({
+                mensagem: "GET pelo nome feito com sucesso.",
+                nomeContato
+            });
+        }
+    })
+}
+
+const getById = (request, response) => {
+    const id = request.params.id;
+
+    contatoCollections.findById(id, (error, contato) => { //a funcao findById(do mongo) pega o os dados ligados ao id passado
+        if(error){
+            return response.status(500).send(error);
+        }else if(contato){
+            return response.status(200).send({
+                mensagem: "GET pelo id feito com sucesso.",
+                contato
+            });
+        }else{
+            return response.status(404).send("O id informado não foi encontrado."); 
+        }
+    })
+}
+
+const deleteContatoById = (request, response) => {
+    const idParam = request.params.id;
+
+    contatoCollections.findByIdAndDelete(idParam, (error, contatoID)=>{
+        if(error){
+            return response.status(500).send(error)
+        }else if(contatoID){
+            return response.status(200).send({mensagem:"Contato deletado com sucesso."})
+        }else{
+            return response.status(404).send("O contato informado não existe.")
+        }
+    })
+}
+
 module.exports = {
     getAll,
-    addContato
+    addContato,
+    getByNome,
+    getById,
+    deleteContatoById
 }
--- api/src/routes/contatosRoute.js
+++ api/src/routes/contatosRoute.js
@@ -5,5 +5,10 @@ const controller = require("../controller/contatosController");
 
 router.get("/", controller.getAll);
 router.post("/criar", controller.addContato);
+router.get("/nome/:nome", controller.getByNome);
+router.get("/id/:id", controller.getById);
+router.delete("/deletar/:id", controller.deleteContatoById); //deletar pelo id
+router.put("/atualizar/telefone/:id",); //atualizar apenas o telefone
+router.patch("atualizar/:id",); //atualizar o contato completo menos o id
 
 module.exports = router;
